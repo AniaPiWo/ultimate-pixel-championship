d1cf954749e657fa72644cb333885780bd5922f7
AniaPiWo <snowflake@wp.pl>
2023-05-27 12:45:58 +0200

    some form css

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,15 +5,25 @@ export const Form = () => {
       <>
          <div className="formBox">
             <div>
-               <p>chosen fighter</p>
-               <p>Sikaczu</p>
+               <p className="chosenFighter">chosen fighter</p>
+               <p className="chosenFighterName">Sikaczu</p>
             </div>
-            <form>
-               <label htmlFor="fighterInput">chosen fighter</label>
-               <input type="text" id="fighterInput" placeholder="Your name" />
-               <label htmlFor="fighterInput">chosen fighter</label>
-               <input type="text" id="fighterInput" placeholder="Your name" />
+            <form className="form">
+               <label htmlFor="fighterInput" className="chosenFighter">
+                  commander
+               </label>
+               <input type="text" className="fighterInput" placeholder="Your name" />
+               <label htmlFor="fighterInput" className="chosenFighter">
+                  email
+               </label>
+               <input type="text" className="fighterInput" placeholder="Your email" />
+               <img
+                  className="submitBtn"
+                  src="./src/assets/img/submitBtn.png"
+                  alt="submit button"
+               />
             </form>
+            <img className="backBtn" src="./src/assets/img/backBtn.png" alt="back button" />
          </div>
       </>
    );
