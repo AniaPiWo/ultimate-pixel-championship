6a83181690a1f9afa74124242dab7eb8a531be7a
AniaPiWo <snowflake@wp.pl>
2023-05-27 14:20:36 +0200

    validation

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,11 +12,17 @@ export const Form = ({ chosenFighter, confirmBtn, backBtn }) => {
                <label htmlFor="fighterInput" className="chosenFighter">
                   commander
                </label>
-               <input type="text" className="fighterInput" placeholder="Your name" />
+               <input type="text" className="fighterInput" placeholder="Your name" required />
                <label htmlFor="fighterInput" className="chosenFighter">
                   email
                </label>
-               <input type="text" className="fighterInput" placeholder="Your email" />
+               <input
+                  type="text"
+                  className="fighterInput"
+                  placeholder="Your email"
+                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$"
+                  required
+               />
                <button type="submit" className="submitBtn" onSubmit={confirmBtn}>
                   <img src="./src/assets/img/submitBtn.png" alt="submit button" />
                </button>
