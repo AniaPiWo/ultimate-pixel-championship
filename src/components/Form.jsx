9322b2ccb47167c399dffe3cd25dabe295168998
AniaPiWo <snowflake@wp.pl>
2023-06-03 13:28:56 +0200

    1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
 
 export const Form = ({ chosenFighter, backBtn, openConfirmation }) => {
    const [email, setEmail] = useState('');
