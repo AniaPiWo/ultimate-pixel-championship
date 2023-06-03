b75eff616936cc66c2bff142d18dbbb6da64c227
AniaPiWo <snowflake@wp.pl>
2023-06-03 14:48:07 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default {
    plugins: [react()],
    base: '/ultimate-pixel-championship/',
    build: {
       outDir: 'dist',
    },
-});
+};
