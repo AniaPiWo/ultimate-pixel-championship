b5a3e737fe8b44127ee8df8d21da5a5790c91392
AniaPiWo <snowflake@wp.pl>
2023-06-04 12:33:37 +0200

    fix3

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { Fighter } from './components/Fighter.jsx';
 import { Form } from './components/Form.jsx';
 import { Confirmation } from './components/Confirmation.jsx';
 import { fighters } from '../fighters.js';
-import prevBtnImg from '../assets/prevBtn.png';
+/* import prevBtnImg from '../assets/prevBtn.png';
 import nextBtnImg from '../assets/nextBtn.png';
 import stepImg1 from '../assets/Step1.png';
 import stepImg2 from '../assets/Step2.png';
-import stepImg3 from '../assets/Step3.png';
+import stepImg3 from '../assets/Step3.png'; */
 import './App.css';
 
 function App() {
