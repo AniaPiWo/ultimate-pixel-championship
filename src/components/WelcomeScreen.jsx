0648f83c209ae18b0d4caf145bb405eb3646537a
AniaPiWo <snowflake@wp.pl>
2023-06-04 12:12:47 +0200

    img test

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,21 +1,24 @@
 import '/src/App.css';
 import PropTypes from 'prop-types';
+import chooseBtn from '../assets/chooseBtn.png';
 
 export const WelcomeScreen = ({ startBtn }) => {
+   const swordsImg = './src/assets/Swords.png';
+
    return (
       <>
          <div className="welcomeBox">
             <h1 className="welcomeTitleTop">
                Ultimate Pix <span className="welcomeTitleBottom">Championships</span>
             </h1>
-            <img src="./src/assets/Swords.png" />
+            <img src={import.meta.resolve(swordsImg)} alt="crossed swords" />
             <p className="welcomeText">
                Fill out the form to sign up
                <br />
                for upcoming tournee.
             </p>
             <button className="startBtn" onClick={startBtn}>
-               <img src="./src/assets/chooseBtn.png" alt="start button" />
+               <img src={chooseBtn} alt="start button" />
             </button>
          </div>
       </>
